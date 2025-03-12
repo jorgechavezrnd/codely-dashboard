9f35eb3e3662d74912bc840cb4d7808e15fe229f
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-12 00:16:07 -0400

    Add component for Dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig((configEnv) => {
 	const isDevelopment = configEnv.mode === "development";
+	const env = loadEnv(configEnv.mode, process.cwd(), '');
 
 	return {
+		define: {
+			'process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN': JSON.stringify(env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN)
+		},
 		plugins: [react()],
 		css: {
 			modules: {
