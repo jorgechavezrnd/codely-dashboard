16970cc3e47203a1f0dcdee9e08a717c3b1375cd
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-12 08:38:38 -0400

    Add base structure for widget list

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig((configEnv) => {
 	const isDevelopment = configEnv.mode === "development";
@@ -9,7 +10,7 @@ export default defineConfig((configEnv) => {
 		define: {
 			'process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN': JSON.stringify(env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN)
 		},
-		plugins: [react()],
+		plugins: [react(), svgr()],
 		css: {
 			modules: {
 				generateScopedName: isDevelopment ? "[name]__[local]__[hash:base64:5]" : "[hash:base64:5]",
