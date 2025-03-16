7cace020ebe6e27cb5f7cb527b24e68c27c2492a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-15 21:09:07 -0400

    Add GitHubRepositoryWidget in order to separte the elements of the dashboard

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -2,34 +2,10 @@ import { useEffect, useState } from 'react';
 
 import { config } from '../../devdash_config';
 import { GitHubRepository } from '../../domain/GitHubRepository';
+import { GitHubRepositoryRepository } from '../../domain/GitHubRepositoryRepository';
 import styles from "./Dashboard.module.scss";
 import Brand from "./brand.svg";
-import Check from "./check.svg";
-import Error from "./error.svg";
-import PullRequests from "./git-pull-request.svg";
-import IssueOpened from "./issue-opened.svg";
-import Lock from "./lock.svg";
-import Forks from "./repo-forked.svg";
-import Start from "./star.svg";
-import Unlock from "./unlock.svg";
-import Watchers from "./watchers.svg";
-import { GitHubRepositoryRepository } from '../../domain/GitHubRepositoryRepository';
-
-const isoToReadableDate = (lastUpdateDate: Date): string => {
-  const currentDate = new Date();
-  const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
-  const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
-
-  if (diffDays === 0) {
-    return "today";
-  }
-
-  if (diffDays > 30) {
-    return "more than a month ago";
-  }
-
-  return `${diffDays} days ago`;
-};
+import { GitHubRepositoryWidget } from './GitHubRepositoryWidget';
 
 export function Dashboard({ repository }: { repository: GitHubRepositoryRepository }) {
   const [repositoryData, setRepositoryData] = useState<GitHubRepository[]>([]);
@@ -57,58 +33,10 @@ export function Dashboard({ repository }: { repository: GitHubRepositoryReposito
       ): (
         <section className={styles.container}>
           {repositoryData.map((widget) => (
-            <article className={styles.widget} key={`${widget.id.organization}/${widget.id.name}`}>
-              <header className={styles.widget__header}>
-                <h2 className={styles.widget__title}>
-                  <a
-                    href={widget.url}
-                    target="_blank"
-                    title={`${widget.id.organization}/${widget.id.name}`}
-                    rel="noreferrer"
-                  >
-                    {widget.id.organization}/{widget.id.name}
-                  </a>
-                </h2>
-                {widget.private ? <img src={Lock} alt='' /> : <img src={Unlock} alt='' />}
-              </header>
-              <div className={styles.widget__body}>
-                <div className={styles.widget__status}>
-                  <p>Last update {isoToReadableDate(widget.updatedAt)}</p>
-                  {widget.hasWorkflows && (
-                    <div>
-                      {widget.isLastWorkflowSuccess ? (
-                        <img src={Check} alt='' />
-                      ) : (
-                        <img src={Error} alt='' />
-                      )}
-                    </div>
-                  )}
-                </div>
-                <p className={styles.widget__description}>{widget.description}</p>
-              </div>
-              <footer className={styles.widget__footer}>
-                <div className={styles.widget__stat}>
-                  <img src={Start} alt='' />
-                  <span>{widget.stars}</span>
-                </div>
-                <div className={styles.widget__stat}>
-                  <img src={Watchers} alt='' />
-                  <span>{widget.watchers}</span>
-                </div>
-                <div className={styles.widget__stat}>
-                  <img src={Forks} alt='' />
-                  <span>{widget.forks}</span>
-                </div>
-                <div className={styles.widget__stat}>
-                  <img src={IssueOpened} alt='' />
-                  <span>{widget.issues}</span>
-                </div>
-                <div className={styles.widget__stat}>
-                  <img src={PullRequests} alt='' />
-                  <span>{widget.pullRequests}</span>
-                </div>
-              </footer>
-            </article>
+            <GitHubRepositoryWidget
+              key={`${widget.id.organization}/${widget.id.name}`}
+              widget={widget}
+            />
           ))}
         </section>
       )}
