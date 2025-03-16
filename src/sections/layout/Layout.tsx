f5b37fb3a3b73d6d5addd4f7de551f53791fb1e0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-16 16:14:02 -0400

    Add link to go back to home from detail

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import Brand from "./brand.svg";
 import { ErrorBoundary } from './ErrorBoundary';
@@ -9,8 +9,12 @@ export function Layout() {
     <>
       <header className={styles.header}>
         <section className={styles.header__container}>
-          <img src={Brand} alt='' />
-          <h1 className={styles.app__brand}>DevDash_</h1>
+          <a href='https://codely.com'>
+            <img src={Brand} alt='' />
+          </a>
+          <Link to={`/`}>
+            <h1 className={styles.app__brand}>DevDash_</h1>
+          </Link>
         </section>
       </header>
       <ErrorBoundary>
