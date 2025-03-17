ba46a519c37a7d825981d563aa08b9ba6bd5d8b9
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-16 20:24:36 -0400

    Fix lint in Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,26 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { DashboardFactory } from './sections/dashboard/DashboardFactory';
-import { Layout } from './sections/layout/Layout';
-import { GitHubRepositoryDetailFactory } from './sections/gitHubRepositoryDetail/GitHubRepositoryDetailFactory';
+import { DashboardFactory } from "./sections/dashboard/DashboardFactory";
+import { GitHubRepositoryDetailFactory } from "./sections/gitHubRepositoryDetail/GitHubRepositoryDetailFactory";
+import { Layout } from "./sections/layout/Layout";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: DashboardFactory.create(),
-      },
-      {
-        path: '/repository/:organization/:name',
-        element: GitHubRepositoryDetailFactory.create(),
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: DashboardFactory.create(),
+			},
+			{
+				path: "/repository/:organization/:name",
+				element: GitHubRepositoryDetailFactory.create(),
+			},
+		],
+	},
 ]);
 
 export function Router() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
