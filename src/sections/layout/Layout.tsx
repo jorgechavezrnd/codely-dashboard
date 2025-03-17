86bdfdbbab5db8a1bd4092cd6a3e1b6a78d7c329
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-16 21:08:28 -0400

    Add progress bar

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -3,10 +3,12 @@ import { Link, Outlet } from "react-router-dom";
 import Brand from "./brand.svg";
 import { ErrorBoundary } from "./ErrorBoundary";
 import styles from "./Layout.module.scss";
+import TopBarProgressByLocation from "./TopBarProgressByLocation";
 
 export function Layout() {
 	return (
 		<>
+			<TopBarProgressByLocation />
 			<header className={styles.header}>
 				<section className={styles.header__container}>
 					<a href="https://codely.com">
