48e0b9eb3dc0d6c4a719937de6db9ff66dde24d1
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-16 20:05:16 -0400

    Add skeleton pattern for widgets

--- src/sections/dashboard/useGitHubRepositories.ts
+++ src/sections/dashboard/useGitHubRepositories.ts
@@ -1,20 +1,27 @@
 import { useEffect, useState } from 'react';
+
 import { GitHubRepository } from '../../domain/GitHubRepository';
 import { GitHubRepositoryRepository } from '../../domain/GitHubRepositoryRepository';
 
 export function useGitHubRepositories(
   repository: GitHubRepositoryRepository,
   repositoriesUrls: string[]
-): { repositoryData: GitHubRepository[] } {
+): {
+  repositoryData: GitHubRepository[],
+  isLoading: boolean
+} {
   const [repositoryData, setRepositoryData] = useState<GitHubRepository[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+      setIsLoading(true);
       repository
         .search(repositoriesUrls)
         .then((repositoryData) => {
           setRepositoryData(repositoryData);
+          setIsLoading(false);
         });
     }, [repository, repositoriesUrls]);
 
-    return { repositoryData };
+    return { repositoryData, isLoading };
 }
