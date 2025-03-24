4ffb5ae5d5376cc7ffbb746078722c6b7c916179
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-03-23 21:45:26 -0400

    Add logic for get repositories from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { LocalStorageRepositoryWidgetRepository } from "./infrastructure/LocalStorageWidgetRepository";
 import { Router } from "./Router";
 import { RepositoryWidgetContextProvider } from "./sections/dashboard/repositoryWidget/RepositoryWidgetContextProvider";
 
+const repository = new LocalStorageRepositoryWidgetRepository();
+
 export function App() {
 	return (
-		<RepositoryWidgetContextProvider>
+		<RepositoryWidgetContextProvider repository={repository}>
 			<Router />
 		</RepositoryWidgetContextProvider>
 	);
